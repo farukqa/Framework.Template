db483130fc474c3862b1a75df1bbf809a0b715d8
LanceW <lawong@mmhayes.com>
2022-03-07 17:10:37 -0500

    use --skip-worktree to ignore files

--- after_clone.sh
+++ after_clone.sh
@@ -1,6 +1,7 @@
+# adds an additional gitconfig file
 git config --local include.path ../.gitconfig
-git init-ignores
+# enables ignoring of select project files
+git ignore-proj-files
+# defines local user info
 git set-user $1
 git set-email $2
-git show-user
-pause
